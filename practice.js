b82e2a8e40d5d6bc58fdc9ca26033f6a84afc09b
Dana Zack <danaezack@gmail.com>
2023-08-27 15:53:46 -0600

    Add all practice information

---
+++ practice.js
@@ -0,0 +1,16 @@
+var firstName = "Dana"
+var lastName = "Zack"
+
+var year = 2023
+var age = 75
+
+var hungry = true
+var depositPaid = true
+
+console.log(firstName)
+console.log(year)
+console.log(depositPaid)
+
+depositPaid = false
+
+console.log(depositPaid)
